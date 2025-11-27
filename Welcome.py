e9e8347c2d41ad8e85a6e301262bbea5635413f5
Anthony-Vdls <a.compsci@proton.me>
2025-11-27 08:31:07 -0700

    typos fixed

--- Welcome.py
+++ Welcome.py
@@ -12,9 +12,9 @@ st.markdown('---')
 st.markdown(
     """
     ## Navigate
-    Thank you for visiting. Navigate this page by the left sidebar.  
+    Thank you for visiting. Navigate this page by the left sidebar (>> on mobile).  
     There you will find:  
-    - **💽About the Data** - Where it came from, what I am to do.
+    - **💽About the Data** - Where it came from, what the aim is.
     - **🧠The Model** - The Machine Learning model, how it was made, and the features.
     - **🎲See Your Chances** - Make predictions with this model!
     """
@@ -24,7 +24,7 @@ st.markdown('---')
 st.markdown(
     """
     ## Objective
-     The objective of this machine learning model is to predict the probability of a recent batchelor graduate getting a job in there field within a year after graduation. This websites aim is to guide you through the begining to the end of the making of this model, starting with an introduction to the data, then the building of the model, and finally using the model to make predictions.
+     The objective of this machine learning model is to predict the probability of a recent batchelor graduate getting a job in their field within a year after graduation. This website's aim is to guide you through the begining to the end of the making of this model, starting with an introduction to the data, then the building of the model, and finally using the model to make predictions.
 
     """
 )
